5e794ff27a5359c69a30dd7b4bf54cd8ecf08163
k13 <mihome013@gmail.com>
2025-07-30 00:28:19 +0330

    read dataset done

--- src/main.py
+++ src/main.py
@@ -1,6 +1,26 @@
 import os
 
 
+# ------------- cleaning the dataset -------------
+def create_block(data):
+    clean_xml = []
+    for line in data.splitlines():
+        strriped = line.strip()
+        if strriped:
+            clean_xml.append(strriped)
+    data = "".join(clean_xml)
+    xml_block = (
+        "<top>\n"
+        + data.replace("<num>", "  <num>")
+        .replace("<title>", "\n  </num>\n  <title>")
+        .replace("<desc>", "\n  </title>\n  <desc>")
+        .replace("<narr>", "\n  </desc>\n  <narr>")
+        .replace("</top>", "\n  </narr>\n</top>")
+        + "\n"
+    )
+    return xml_block
+
+
 def read_dataset(file_path):
     with open(file_path, mode="r", encoding="utf-8") as file:
         dataset = file.read()
@@ -9,15 +29,7 @@ def read_dataset(file_path):
     for block in dataset_blocks:
         if "<num>" not in block:
             continue
-        xml_block = (
-            "<top>\n"
-            + block.replace("<num>", "  <num>")
-            .replace("<title>", "\n  </num>\n  <title>")
-            .replace("<desc>", "\n  </title>\n  <desc>")
-            .replace("<narr>", "\n  </desc>\n  <narr>")
-            .replace("</top>", "\n  </narr>\n</top>")
-            + "\n"
-        )
+        xml_block = create_block(block)
         xml_dataset.append(xml_block)
     xml_dataset = "<root>\n" + "".join(xml_dataset) + "\n </root>"
     dir_name, base_name = os.path.split(file_path)
@@ -30,3 +42,4 @@ def read_dataset(file_path):
 read_dataset(
     "/home/kk_gorbee/Documents/project/InternetRetrieval/BasicOperations/dataset/04.testset"
 )
+# -------------  -------------
